3907d3cf923242c70bd76ff188118e636e3131d4
Unknown <arthur.koalas@free.fr>
2018-07-16 00:36:43 +0200

    all default color paint pot are added

--- src/main/java/com/arthurbambou/paintingmod/init/ModItems.java
+++ src/main/java/com/arthurbambou/paintingmod/init/ModItems.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.arthurbambou.paintingmod.items.ItemBase;
+import com.arthurbambou.paintingmod.items.PaintPot;
 import com.arthurbambou.paintingmod.items.Paintbrush;
 import com.arthurbambou.paintingmod.items.WaterVial;
 import com.arthurbambou.paintingmod.items.tools.ToolPickaxe;
@@ -23,9 +24,29 @@ public class ModItems
 	
 	//Items
 	public static final Item PAINTBRUSH = new Paintbrush("paintbrush");
+	
 	public static final Item EARTH_MORTAR = new ItemBase("earth_mortar");
+	
 	public static final Item WATER_VIAL = new WaterVial("water_vial", 12, 64);
 	public static final Item DOUBLE_WATER_VIAL = new WaterVial("double_water_vial", 24, 32);
+	
+	public static final Item BLACK_POT = new PaintPot("black_paint_pot");
+	public static final Item RED_POT = new PaintPot("red_paint_pot");
+	public static final Item GREEN_POT = new PaintPot("green_paint_pot");
+	public static final Item BROWN_POT = new PaintPot("brown_paint_pot");
+	public static final Item BLUE_POT = new PaintPot("blue_paint_pot");
+	public static final Item PURPLE_POT = new PaintPot("purple_paint_pot");
+	public static final Item CYAN_POT = new PaintPot("cyan_paint_pot");
+	public static final Item LIGHT_GRAY_POT = new PaintPot("light_gray_paint_pot");
+	public static final Item GRAY_POT = new PaintPot("gray_paint_pot");
+	public static final Item PINK_POT = new PaintPot("pink_paint_pot");
+	public static final Item LIME_POT = new PaintPot("lime_paint_pot");
+	public static final Item YELLOW_POT = new PaintPot("yellow_paint_pot");
+	public static final Item LIGHT_BLUE_POT = new PaintPot("light_blue_paint_pot");
+	public static final Item MAGENTA_POT = new PaintPot("magenta_paint_pot");
+	public static final Item ORANGE_POT = new PaintPot("orange_paint_pot");
+	public static final Item WHITE_POT = new PaintPot("white_paint_pot");
+	
 	
 	//Tools
 	public static final ItemPickaxe HAMMER = new ToolPickaxe("hammer", MATERIAL_HAMMER);
