40554906cfe25fcd0662fce7b52c7acd90f1ddcc
Unknown <arthur.koalas@free.fr>
2018-07-15 15:21:52 +0200

    Custom drop for the soggy clay

--- src/main/java/com/arthurbambou/paintingmod/blocks/SoggyClay.java
+++ src/main/java/com/arthurbambou/paintingmod/blocks/SoggyClay.java
@@ -1,7 +1,14 @@
 package com.arthurbambou.paintingmod.blocks;
 
+import java.util.Random;
+
+import com.arthurbambou.paintingmod.init.ModBlocks;
+import com.arthurbambou.paintingmod.init.ModItems;
+
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
 
 public class SoggyClay extends BlockBase
 {
@@ -14,5 +21,30 @@ public class SoggyClay extends BlockBase
 		setResistance(3.0F);
 		setHarvestLevel("pickaxe", 1);
 	}
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return ModItems.EARTH_MORTAR;
+    }
+
+    @Override
+    public int quantityDropped(Random random){
+    	int amount = 0;
+    	amount = 2 + random.nextInt(2);
+        return amount; 
+    }
+
+    @Override
+    public int quantityDroppedWithBonus(int fortune, Random random){
+        if(fortune > 0 && Item.getItemFromBlock(this) != this.getItemDropped((IBlockState)this.getBlockState().getValidStates().iterator().next(), random, fortune)){
+            int i = random.nextInt(fortune + 2) - 1;
+
+            if(i < 1){
+                i = 1;
+            }
+            return this.quantityDropped(random) * (i);
+        } else {
+            return this.quantityDropped(random);
+        }
+    }
 
 }
