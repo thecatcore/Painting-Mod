0ede0349f3cb6f20c292b9dae7540e4a65f144b0
Unknown <arthur.koalas@free.fr>
2018-07-16 02:34:36 +0200

    [WIP]Water Vials

--- src/main/java/com/arthurbambou/paintingmod/items/WaterVial.java
+++ src/main/java/com/arthurbambou/paintingmod/items/WaterVial.java
@@ -1,10 +1,24 @@
 package com.arthurbambou.paintingmod.items;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
 public class WaterVial extends ItemBase {
+	int stack = 1;
 	public WaterVial(String name, int damage, int stack, CreativeTabs materials) {
 		super(name, materials);
 		
 		setMaxDamage(damage);
 		setMaxStackSize(stack);
 	}
+	
+	@Override
+    public boolean hasContainerItem(ItemStack stack){
+        return true;
+    }
+
+    @Override
+    public ItemStack getContainerItem(ItemStack itemStack) {
+        return new ItemStack(this, 1, 1);
+    }
 }
