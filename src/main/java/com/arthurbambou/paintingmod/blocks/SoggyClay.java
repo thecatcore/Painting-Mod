2086d38f70d80aa494f88dfdd24172d4cb30a52c
Unknown <arthur.koalas@free.fr>
2018-07-16 18:30:34 +0200

    new way of creating earth mortar.

--- src/main/java/com/arthurbambou/paintingmod/blocks/SoggyClay.java
+++ src/main/java/com/arthurbambou/paintingmod/blocks/SoggyClay.java
@@ -2,13 +2,25 @@ package com.arthurbambou.paintingmod.blocks;
 
 import java.util.Random;
 
+import javax.annotation.Nullable;
+
 import com.arthurbambou.paintingmod.init.ModBlocks;
 import com.arthurbambou.paintingmod.init.ModItems;
 
+import net.minecraft.block.BlockAnvil;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import net.minecraftforge.oredict.OreDictionary;
 
 public class SoggyClay extends BlockBase
 {
@@ -23,7 +35,7 @@ public class SoggyClay extends BlockBase
 	}
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune){
-        return ModItems.EARTH_MORTAR;
+        return Items.CLAY_BALL;
     }
 
     @Override
@@ -45,6 +57,20 @@ public class SoggyClay extends BlockBase
         } else {
             return this.quantityDropped(random);
         }
+    }
+    
+    public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        if (!worldIn.isRemote)
+        {
+        }
+        ItemStack item = playerIn.getHeldItemMainhand();
+        Item itemm = item.getItem();
+        if(itemm == ModItems.HAMMER) {
+        	worldIn.setBlockToAir(pos);
+        	playerIn.inventory.addItemStackToInventory(new ItemStack(ModItems.EARTH_MORTAR, 4));
+        }
+        return true;
     }
 
 }
