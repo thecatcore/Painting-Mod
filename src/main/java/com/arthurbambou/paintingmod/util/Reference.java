42d145912ed0e53807fa06f1336134e9d4971202
Unknown <arthur.koalas@free.fr>
2018-08-03 00:04:20 +0200

    Fix crash ?

--- src/main/java/com/arthurbambou/paintingmod/util/Reference.java
+++ src/main/java/com/arthurbambou/paintingmod/util/Reference.java
@@ -1,10 +1,10 @@
 package com.arthurbambou.paintingmod.util;
 
 public class Reference {
-	
+	public static final String VVERSION= System.getenv("TRAVIS_TAG") + "-beta";
 	public static final String MOD_ID= "paintingmod";
 	public static final String NAME= "Painting Mod";
-	public static final String VERSION= System.getenv("TRAVIS_TAG") + "-beta";
+	public static final String VERSION= VVERSION;
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.arthurbambou.paintingmod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.arthurbambou.paintingmod.proxy.CommonProxy";
