5ad6337120481028e16baddcc49891946382c4c6
arthurbambou <34719527+arthurbambou@users.noreply.github.com>
2018-08-03 18:25:24 +0200

    Colored Paintbrush and Creative Tab (#22)
    * add sub-item to colored paintbrush
    
    * Change for sub-items id
    
    * fix lang
    
    * space
    
    * Change technique
    
    * add paintbrushes in the code
    
    * Models
    
    * Textures
    
    * Recipe
    
    * New name for the default paintbrush
    
    * langs files
    
    * A CreativeTab for all the mod
    
    * Recipe 2
    
    * Remove unused import

--- src/main/java/com/arthurbambou/paintingmod/blocks/BlockBase.java
+++ src/main/java/com/arthurbambou/paintingmod/blocks/BlockBase.java
@@ -14,12 +14,12 @@ import net.minecraft.item.ItemBlock;
 public class BlockBase extends Block implements IHasModel
 {
 
-	public BlockBase(String name, Material material)
+	public BlockBase(String name, Material material, CreativeTabs tab)
 	{
 		super(material);
 		setUnlocalizedName(name);
 		setRegistryName(name);
-		setCreativeTab(CreativeTabs.BUILDING_BLOCKS);
+		setCreativeTab(tab);
 		
 		ModBlocks.BLOCKS.add(this);
 		ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
