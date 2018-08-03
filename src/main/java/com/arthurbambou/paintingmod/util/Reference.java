7dc5fecb152e3c920dc7f07bab68cb57cf04d10d
Unknown <arthur.koalas@free.fr>
2018-08-03 13:23:01 +0200

    Version...

--- src/main/java/com/arthurbambou/paintingmod/util/Reference.java
+++ src/main/java/com/arthurbambou/paintingmod/util/Reference.java
@@ -3,6 +3,7 @@ package com.arthurbambou.paintingmod.util;
 public class Reference {
 	public static final String MOD_ID= "paintingmod";
 	public static final String NAME= "Painting Mod";
+	public static final String VERSION = "0.6.3"
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.arthurbambou.paintingmod.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.arthurbambou.paintingmod.proxy.CommonProxy";
