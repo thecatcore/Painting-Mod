09595ca16f230f343065fae1823add5b2c911a99
Unknown <arthur.koalas@free.fr>
2018-08-04 22:11:28 +0200

    Codacy I

--- src/main/java/com/arthurbambou/paintingmod/items/Paintbrush.java
+++ src/main/java/com/arthurbambou/paintingmod/items/Paintbrush.java
@@ -1,21 +1,16 @@
 package com.arthurbambou.paintingmod.items;
 
-import com.arthurbambou.paintingmod.Main;
 import com.arthurbambou.paintingmod.init.ModBlocks;
 import com.arthurbambou.paintingmod.init.ModItems;
 
 import net.minecraft.block.Block;
-import net.minecraft.block.state.BlockStateContainer;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
