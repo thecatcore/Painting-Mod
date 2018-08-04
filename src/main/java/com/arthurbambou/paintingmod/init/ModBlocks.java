6971d449374b57fbc445b87fb2127ee1c3f3bd11
Unknown <arthur.koalas@free.fr>
2018-08-04 17:57:46 +0200

    Add planks with code

--- src/main/java/com/arthurbambou/paintingmod/init/ModBlocks.java
+++ src/main/java/com/arthurbambou/paintingmod/init/ModBlocks.java
@@ -34,34 +34,34 @@ public class ModBlocks {
 	
 	
 	//Black Blocks
-	
+	public static final Block BLACK_PLANK = new BlackBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Red Blocks
-	
+	public static final Block RED_PLANK = new RedBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Green Blocks
-	
+	public static final Block GREEN_PLANK = new GreenBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Brown Blocks
-	
+	public static final Block BROWN_PLANK = new BrownBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Blue Blocks
-	
+	public static final Block BLUE_PLANK = new BlueBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Purple Blocks
-	
+	public static final Block PURPLE_PLANK = new PurpleBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Cyan Blocks
-	
+	public static final Block CYAN_PLANK = new CyanBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Light Gray Blocks
-	
+	public static final Block LIGHT_GRAY_PLANK = new LightGrayBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Gray Blocks
-	
+	public static final Block GRAY_PLANK = new GrayBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Pink Blocks
-	
+	public static final Block PINK_PLANK = new PinkBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Lime Blocks
-	
+	public static final Block LIME_PLANK = new LimeBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Yellow Blocks
-	
+	public static final Block YELLOW_PLANK = new YellowBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Light Blue Blocks
-	
+	public static final Block LIGHT_BLUE_PLANK = new LightBlueBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Magenta Blocks
-	
+	public static final Block MAGENTA_PLANK = new MagentaBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//Orange Blocks
-	
+	public static final Block WHITE_PLANK = new WhiteBlock("plank", Material.WOOD, SoundType.WOOD, 2.0F, 15.0F, null, 0);
 	//White Blocks
 }
