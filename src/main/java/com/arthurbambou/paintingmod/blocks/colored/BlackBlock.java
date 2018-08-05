746ee0fb630cf5f914899263a564d30f8adb6d27
Unknown <arthur.koalas@free.fr>
2018-08-05 14:41:38 +0200

    Space

--- src/main/java/com/arthurbambou/paintingmod/blocks/colored/BlackBlock.java
+++ src/main/java/com/arthurbambou/paintingmod/blocks/colored/BlackBlock.java
@@ -5,10 +5,12 @@ import com.arthurbambou.paintingmod.blocks.BlockBase;
 
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 
 public class BlackBlock extends BlockBase
 {
-
 	public BlackBlock(String name, Material material, SoundType sound, float hardness, float resistance, String harvestTool, int harvestLevel)
 	{
 		super("black_" + name, material, Main.PAINTING_MOD);
