9b992451d2e9d91af662607ecbc02b2781bf0c72
Unknown <arthur.koalas@free.fr>
2018-08-06 17:06:27 +0200

    Add a very uselful js script
    Use: 
    -create all necessairy models and blockstates files for a colored block

---
+++ block_resourcesforjson.js
@@ -0,0 +1,73 @@
+var fs = require("fs");
+var blockname = "stone"
+var color = [
+    "black",
+    "blue",
+    "brown",
+    "cyan",
+    "gray",
+    "green",
+    "light_blue",
+    "light_gray",
+    "lime",
+    "magenta",
+    "orange",
+    "pink",
+    "purple",
+    "red",
+    "white",
+    "yellow"
+]
+for (var i=0; i<color.length;i++) {
+blockstate(i)
+blockmodel(i)
+itemblockmodel(i)
+}
+
+function blockstate (i) {
+    var path = `./src/main/resources/assets/paintingmod/blockstates/${color[i]}_${blockname}.json`
+    console.log(`Création en cours du fichier: ${path}`)
+    var createStream = fs.createWriteStream(path);
+    createStream.end();
+    var json = {
+        variants:{
+            normal:{
+                model:`paintingmod:${color[i]}_${blockname}`
+            }
+        }
+    }
+    fs.writeFile(path, JSON.stringify(json), (err) => {
+        if (err) throw err;
+        console.log('Le fichier a été sauvegardé!');
+    });
+}
+
+function blockmodel (i) {
+    var path = `./src/main/resources/assets/paintingmod/models/block/${color[i]}_${blockname}.json`
+    console.log(`Création en cours du fichier: ${path}`)
+    var createStream = fs.createWriteStream(path);
+    createStream.end();
+    var json = {
+        parent: "block/cube_all",
+        textures: {
+            all: `paintingmod:blocks/${color[i]}_${blockname}`
+        }
+    }
+    fs.writeFile(path, JSON.stringify(json), (err) => {
+        if (err) throw err;
+        console.log('Le fichier a été sauvegardé!');
+    });
+}
+function itemblockmodel (i) {
+    var path = `./src/main/resources/assets/paintingmod/models/item/${color[i]}_${blockname}.json`
+    console.log(`Création en cours du fichier: ${path}`)
+    var createStream = fs.createWriteStream(path);
+    createStream.end();
+    var json = {
+        parent: `paintingmod:block/${color[i]}_${blockname}`
+    }
+    fs.writeFile(path, JSON.stringify(json), (err) => {
+        if (err) throw err;
+        console.log('Le fichier a été sauvegardé!');
+    });
+}
