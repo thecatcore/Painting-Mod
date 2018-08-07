24e3360de4e5fbd916805b993a4c79f4b6a522f6
Unknown <arthur.koalas@free.fr>
2018-08-07 20:51:17 +0200

    The script is better than before

--- block_resourcesforjson.js
+++ block_resourcesforjson.js
@@ -1,12 +1,37 @@
 var fs = require("fs");
 var blocks = [
     {
-        id: "diorite",
-        name: "Diorite",
-        origine: "Blocks.STONE",
+        id: "dirt",
+        name: "Dirt",
+        origine: "Blocks.DIRT",
         usemeta: true,
-        meta: 3,
-        hardness: "1.5F",
+        meta: 0,
+        hardness: "0.5F",
+        resistance: "2.5F",
+        harvestTool: "shovel",
+        harvestLevel: 0,
+        material: "GROUND",
+        sound: "DIRT"
+    },
+    {
+        id: "coarse_dirt",
+        name: "Coarse Dirt",
+        origine: "Blocks.DIRT",
+        usemeta: true,
+        meta: 1,
+        hardness: "0.5F",
+        resistance: "2.5F",
+        harvestTool: "shovel",
+        harvestLevel: 0,
+        material: "GROUND",
+        sound: "DIRT"
+    },
+    {
+        id: "cobblestone",
+        name: "Cobblestone",
+        origine: "Blocks.COBBLESTONE",
+        usemeta: false,
+        hardness: "2.0F",
         resistance: "30.0F",
         harvestTool: "pickaxe",
         harvestLevel: 1,
@@ -55,14 +80,15 @@ var modblocksresult = modblocks
 var paintbrush = fs.readFileSync("./Paintbrush.txt", {encoding:'utf-8', flag:'r'})
 var paintbrushresult = paintbrush
 for (var a = 0; a < blocks.length; a++) {
-//var pathh = `./src/main/resources/assets/paintingmod/lang/en_us.lang`
-//var langfile = fs.readFileSync(pathh, {encoding:'utf-8', flag:'r'}, (err) => {
-//    if (err) throw err;
-//});
-//var test = langfile + "\n\n//" + blocks[a].name
-//fs.writeFile(pathh, test, (err) => {
-//    if (err) throw err;
-//})
+console.log("\n//" + blocks[a].name)
+var pathh = `./src/main/resources/assets/paintingmod/lang/en_us.lang`
+var langfile = fs.readFileSync(pathh, {encoding:'utf-8', flag:'r'}, (err) => {
+   if (err) throw err;
+});
+var test = langfile + "\n\n//" + blocks[a].name
+fs.writeFile(pathh, test, (err) => {
+   if (err) throw err;
+})
 modblocksresult = modblocksresult + "\n\n//" + blocks[a].name
 fs.writeFile("./ModBlocks.txt", modblocksresult, (err) => {
    if (err) throw err;
@@ -72,17 +98,18 @@ fs.writeFile("./Paintbrush.txt", paintbrushresult, (err) => {
     if (err) throw err;
 })
 for (var i=0; i<color.length;i++) {
-// blockstate(i)
-// console.log("")
-// blockmodel(i)
-// console.log("")
-// itemblockmodel(i)
-// console.log("")
-// test = test + "\ntile." + color[i] + "_" + blocks[a].id + ".name=" + Color[i] + " " + blocks[a].name
-// fs.writeFile(pathh, test, (err) => {
-//     if (err) throw err;
-// })
-// console.log("La ligne : " + "\ntile." + color[i] + "_" + blocks[a].id + ".name=" + Color[i] + " " + blocks[a].name + "\na été ajoutée avec succès au fichier de langue")
+blockstate(i)
+console.log("")
+blockmodel(i)
+console.log("")
+itemblockmodel(i)
+console.log("")
+test = test + "\ntile." + color[i] + "_" + blocks[a].id + ".name=" + Color[i] + " " + blocks[a].name
+fs.writeFile(pathh, test, (err) => {
+    if (err) throw err;
+})
+console.log("La ligne : " + "\ntile." + color[i] + "_" + blocks[a].id + ".name=" + Color[i] + " " + blocks[a].name + "\na été ajoutée avec succès au fichier de langue")
+console.log("")
 var staticname = color[i].toUpperCase() + "_" + blocks[a].id.toUpperCase()
 if (i === 7) {
     modblocksresult = modblocksresult + "\npublic static final Block " + staticname + "= new LightGrayBlock(" + `"${blocks[a].id}", Material.${blocks[a].material}, SoundType.${blocks[a].sound}, ${blocks[a].hardness}, ${blocks[a].resistance}, "${blocks[a].harvestTool}", ${blocks[a].harvestLevel});`;
@@ -93,13 +120,14 @@ modblocksresult = modblocksresult + "\npublic static final Block " + staticname 
 }
 fs.writeFile("./ModBlocks.txt", modblocksresult, (err) => {
     if (err) throw err;
- })
+})
+console.log("La ligne de code : " + "\npublic static final Block " + staticname + "= new " + Color[i] + "Block(" + `"${blocks[a].id}", Material.${blocks[a].material}, SoundType.${blocks[a].sound}, ${blocks[a].hardness}, ${blocks[a].resistance}, "${blocks[a].harvestTool}", ${blocks[a].harvestLevel});` + "\nÀ été générée avec succès !")
+console.log("")
+console.log("")
 paintbrushresult = paintbrushresult + ", ModBlocks." + staticname
 fs.writeFile("./Paintbrush.txt", paintbrushresult, (err) => {
     if (err) throw err;
 })
-// console.log("")
-// console.log("")
 }
 if (blocks[a].usemeta === true) {
     paintbrushresult = paintbrushresult + ", " + blocks[a].meta + ");"
@@ -112,6 +140,12 @@ if (blocks[a].usemeta === true) {
         if (err) throw err;
     })
 }
+var splited = paintbrushresult.split("\n")
+console.log("La ligne de code : \n" + splited[splited.length] + "\nÀ été générée avec succès !")
+console.log("")
+console.log("")
+console.log("")
+console.log("")
 }
 
 function blockstate (i) {
