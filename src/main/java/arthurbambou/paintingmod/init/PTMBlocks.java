07c3e71e9bf8c14e5d8938acd16d11c5ac719346
Unknown <arthur.koalas@free.fr>
2018-10-14 11:46:40 +0200

    Add 4 block code
    Quartz Block, Prismarine, Prismarine Brick and Dark Prismarine

--- src/main/java/arthurbambou/paintingmod/init/PTMBlocks.java
+++ src/main/java/arthurbambou/paintingmod/init/PTMBlocks.java
@@ -53,7 +53,10 @@ public class PTMBlocks {
 	public static final ColoredBlock NETHER_BRICK = new ColoredBlock("nether_brick", Material.ROCK, SoundType.STONE, 2.0F, 30.0F, "pickaxe", 1, Blocks.NETHER_BRICK, false);
 	public static final ColoredBlock END_STONE = new ColoredBlock("end_stone", Material.ROCK, SoundType.STONE, 3.0F, 45.0F, "pickaxe", 1, Blocks.END_STONE, false);
 	public static final ColoredBlock EMERALD_BLOCK = new ColoredBlock("emerald_block", Material.ROCK, SoundType.STONE, 5.0F, 30.0F, "pickaxe", 3, Blocks.EMERALD_BLOCK, false);
-	
+	public static final ColoredBlock QUARTZ_BLOCK= new ColoredBlock("quartz_block", Material.ROCK, SoundType.STONE, 0.8F, 4.0F, "pickaxe", 1, Blocks.QUARTZ_BLOCK, true, 0);
+	public static final ColoredBlock PRISMARINE= new ColoredBlock("prismarine", Material.ROCK, SoundType.STONE, 1.5F, 30.0F, "pickaxe", 1, Blocks.PRISMARINE, true, 0);
+	public static final ColoredBlock PRISMARINE_BRICK= new ColoredBlock("prismarine_brick", Material.ROCK, SoundType.STONE, 1.5F, 30.0F, "pickaxe", 1, Blocks.PRISMARINE, true, 1);
+	public static final ColoredBlock DARK_PRISMARINE= new ColoredBlock("dark_prismarine", Material.ROCK, SoundType.STONE, 1.5F, 30.0F, "pickaxe", 1, Blocks.PRISMARINE, true, 2);
 
 //	//Chiseled Sandstone
 //	public static final Block BLACK_CHISELED_SANDSTONE= new PTMBlackBlock("chiseled_sandstone", Material.GROUND, SoundType.STONE, 0.8F, 4.0F, "pickaxe", 1);
