3f602c5ea7b1838db884cf5aaa8e41671e6766a6
Arthur Roussel <34719527+arthurbambou@users.noreply.github.com>
2018-10-24 18:35:34 +0200

    Update AddPaintbrush.java

--- src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
+++ src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
@@ -3,11 +3,13 @@ package arthurbambou.paintingmod.api;
 import java.util.ArrayList;
 import java.util.List;
 
+import arthurbambou.paintingmod.PTMMain;
+
 public class AddPaintbrush {
 	private static final List<ColoredBlock> COLOREDBLOCKS = new ArrayList<ColoredBlock>();
 	
 	public void register(ColoredBlock coloredblock) {
-		
-	     COLOREDBLOCKS.push(coloredblock);
+		PTMMain.logger.info("[API]: Add paintable block : " + coloredblock.name)
+	        COLOREDBLOCKS.push(coloredblock);
 	}
 }
