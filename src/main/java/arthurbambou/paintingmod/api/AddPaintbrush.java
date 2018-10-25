36408d8892a9dd386325bb7644ebd87e3a14f8b2
Arthur Roussel <34719527+arthurbambou@users.noreply.github.com>
2018-10-25 12:51:04 +0200

    Update AddPaintbrush.java

--- src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
+++ src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
@@ -6,10 +6,23 @@ import java.util.List;
 import arthurbambou.paintingmod.PTMMain;
 
 public class AddPaintbrush {
-	private static final List<ColoredBlock> COLOREDBLOCKS = new ArrayList<ColoredBlock>();
+	// Like the Stone
+	private static final List<ColoredBlock> SIMPLECOLOREDBLOCKS = new ArrayList<ColoredBlock>();
+	// Like the Sandstone
+	private static final List<ColoredBlock> MIXTECOLOREDBLOCKS = new ArrayList<ColoredBlock>();
+	// Like the Oak Log
+	private static final List<ColoredBlock> COMPLEXCOLOREDBLOCKS = new ArrayList<ColoredBlock>();
 	
-	public void register(ColoredBlock coloredblock) {
-		PTMMain.logger.info("[API]: Add paintable block : " + coloredblock.name)
-	        COLOREDBLOCKS.push(coloredblock);
+	public void registerSimpleBlock(ColoredBlock coloredblock) {
+		PTMMain.logger.info("[API]: Add paintable simple block : " + coloredblock.name)
+	        SIMPLECOLOREDBLOCKS.push(coloredblock);
+	}
+	public void registerMixteBlock(ColoredBlock coloredblock) {
+		PTMMain.logger.info("[API]: Add paintable mixte block : " + coloredblock.name)
+	        MIXTECOLOREDBLOCKS.push(coloredblock);
+	}
+	public void registerComplexBlock(ColoredBlock coloredblock) {
+		PTMMain.logger.info("[API]: Add paintable complex block : " + coloredblock.name)
+	        COMPLEXCOLOREDBLOCKS.push(coloredblock);
 	}
 }
