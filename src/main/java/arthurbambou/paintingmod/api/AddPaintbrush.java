ecc05cb864090847fe3975dcf7a01a895ea46d7a
arthurbambou <arthur.koalas@free.fr>
2018-10-26 23:34:29 +0200

    add some useful function

--- src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
+++ src/main/java/arthurbambou/paintingmod/api/AddPaintbrush.java
@@ -6,23 +6,26 @@ import java.util.List;
 import arthurbambou.paintingmod.PTMMain;
 
 public class AddPaintbrush {
-	// Like the Stone
+	// Like the Stone, the Sandstone and the Crafting Table
 	private static final List<ColoredBlock> SIMPLECOLOREDBLOCKS = new ArrayList<ColoredBlock>();
-	// Like the Sandstone
-	private static final List<ColoredBlock> MIXTECOLOREDBLOCKS = new ArrayList<ColoredBlock>();
-	// Like the Oak Log
+	// Like the Oak Log, the Furnace and the Dispenser
 	private static final List<ColoredBlock> COMPLEXCOLOREDBLOCKS = new ArrayList<ColoredBlock>();
 	
-	public void registerSimpleBlock(ColoredBlock coloredblock) {
-		PTMMain.logger.info("[API]: Add paintable simple block : " + coloredblock.name)
-	        SIMPLECOLOREDBLOCKS.push(coloredblock);
+	public static void registerSimpleBlock(ColoredBlock coloredblock) {
+		PTMMain.logger.info("[API]: Add paintable simple block : " + coloredblock.name);
+		SIMPLECOLOREDBLOCKS.add(coloredblock);
 	}
-	public void registerMixteBlock(ColoredBlock coloredblock) {
-		PTMMain.logger.info("[API]: Add paintable mixte block : " + coloredblock.name)
-	        MIXTECOLOREDBLOCKS.push(coloredblock);
+
+	public static void registerComplexBlock(ColoredBlock coloredblock) {
+		PTMMain.logger.info("[API]: Add paintable complex block : " + coloredblock.name);
+		COMPLEXCOLOREDBLOCKS.add(coloredblock);
 	}
-	public void registerComplexBlock(ColoredBlock coloredblock) {
-		PTMMain.logger.info("[API]: Add paintable complex block : " + coloredblock.name)
-	        COMPLEXCOLOREDBLOCKS.push(coloredblock);
+
+	public static List<ColoredBlock> getSIMPLECOLOREDBLOCKS() {
+		return SIMPLECOLOREDBLOCKS;
+	}
+
+	public static List<ColoredBlock> getCOMPLEXCOLOREDBLOCKS() {
+		return COMPLEXCOLOREDBLOCKS;
 	}
 }
