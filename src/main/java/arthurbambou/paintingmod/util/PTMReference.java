6fcdf40a68fc664f301f8533b544fa9d876c0157
arthurbambou <arthur.koalas@free.fr>
2018-10-27 14:43:30 +0200

    versionning

--- src/main/java/arthurbambou/paintingmod/util/PTMReference.java
+++ src/main/java/arthurbambou/paintingmod/util/PTMReference.java
@@ -3,7 +3,7 @@ package arthurbambou.paintingmod.util;
 public class PTMReference {
 	public static final String MOD_ID= "paintingmod";
 	public static final String NAME= "Painting Mod";
-	public static final String VERSION = "0.8.2";
+	public static final String VERSION = "@VERSION@";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "arthurbambou.paintingmod.proxy.PTMClientProxy";
 	public static final String COMMON_PROXY_CLASS = "arthurbambou.paintingmod.proxy.PTMCommonProxy";
