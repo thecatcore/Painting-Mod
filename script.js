df63077d92448ee7be9721934aaa9ba9e955336f
Cat Core <arthur.koalas@free.fr>
2018-12-21 18:26:49 +0100

    Fix blockstates 2

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ var files = fs.readdirSync(path, "utf8")
 for(var i = 0; i < files.length; i++) {
     var data = fs.readFileSync(path + "/" + files[i], { encoding:"utf8" })
     var datat = JSON.parse(data)
-    var modelvalue = datat.variants.normal
-    datat.variants = {}
-    datat.variants[""] = modelvalue
     console.log(datat)
     if (datat.variants[""] != undefined) {
+        var modelstring = datat.variants[""].model
+        var modelstringarray = modelstring.split(":")
+        var modelstringarray1 = modelstringarray[0] + ":"
+        var modelstringarray2 = modelstringarray[1]
+        modelstring = modelstringarray1 + "block/" + modelstringarray2
+        datat.variants[""].model = modelstring
         fs.writeFile(path + "/" + files[i], JSON.stringify(datat), (err) => {
             if (err) {
                 console.log(err)
