b100d20b287f1755a6445e8191f2427e5ebc3066
Cat Core <arthur.koalas@free.fr>
2019-01-17 20:33:40 +0100

    small fix

--- src/main/java/arthurbambou/paintingmod/blocks/ColoredFenceBlock.java
+++ src/main/java/arthurbambou/paintingmod/blocks/ColoredFenceBlock.java
@@ -31,7 +31,7 @@ public class ColoredFenceBlock extends FenceBlock {
 
     public boolean method_10184(BlockState blockState_1, boolean boolean_1, Direction direction_1) {
         Block block_1 = blockState_1.getBlock();
-        boolean boolean_2 = block_1 instanceof FenceBlock && blockState_1.getMaterial() == this.material;
+        boolean boolean_2 = block_1 instanceof ColoredFenceBlock && blockState_1.getMaterial() == this.material;
         boolean boolean_3 = block_1 instanceof FenceGateBlock && FenceGateBlock.method_16703(blockState_1, direction_1);
         return !method_10185(block_1) && boolean_1 || boolean_2 || boolean_3;
     }
