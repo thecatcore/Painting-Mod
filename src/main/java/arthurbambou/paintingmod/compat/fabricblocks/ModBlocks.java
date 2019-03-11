8538d0db599434ad4ebb22e709b1ffa6308feaa6
Cat Core <arthur.koalas@free.fr>
2019-03-11 19:19:57 +0100

    small fix

--- src/main/java/arthurbambou/paintingmod/compat/fabricblocks/ModBlocks.java
+++ src/main/java/arthurbambou/paintingmod/compat/fabricblocks/ModBlocks.java
@@ -4,7 +4,7 @@ import arthurbambou.paintingmod.mainmod.api.ColoredBlock;
 import arthurbambou.paintingmod.mainmod.api.ColoredSlab;
 import arthurbambou.paintingmod.mainmod.api.ColoredStairs;
 import arthurbambou.paintingmod.mainmod.api.ColoredWall;
-//import com.yanis48.fabriblocks.FabriBlocks;
+import com.yanis48.fabriblocks.FabriBlocks;
 import net.minecraft.block.Block;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -41,17 +41,17 @@ public class ModBlocks {
 //        SNOW_BRICK_SLAB = new ColoredSlab("snow_brick_slab", com.yanis48.fabriblocks.init.ModBlocks.SNOW_BRICK_SLAB, FabriBlocks.MOD_ID);
 //
 //        SNOW_BRICK_WALL = new ColoredWall("snow_brick_wall", com.yanis48.fabriblocks.init.ModBlocks.SNOW_BRICK_WALL, FabriBlocks.MOD_ID);
-//        STONE_WALL = new ColoredWall("stone_wall", com.yanis48.fabriblocks.init.ModBlocks.STONE_WALL, FabriBlocks.MOD_ID);
+        STONE_WALL = new ColoredWall("stone_wall", com.yanis48.fabriblocks.init.ModBlocks.STONE_WALL, FabriBlocks.MOD_ID);
 //        SMOOTH_STONE_WALL = new ColoredWall("smooth_stone_wall", com.yanis48.fabriblocks.init.ModBlocks.SMOOTH_STONE_WALL, FabriBlocks.MOD_ID);
 //        SMOOTH_SANDSTONE_WALL = new ColoredWall("smooth_sandstone_wall", com.yanis48.fabriblocks.init.ModBlocks.SMOOTH_SANDSTONE_WALL, FabriBlocks.MOD_ID);
 //        SMOOTH_RED_SANDSTONE_WALL = new ColoredWall("smooth_red_sandstone_wall", com.yanis48.fabriblocks.init.ModBlocks.SMOOTH_RED_SANDSTONE_WALL, FabriBlocks.MOD_ID);
-//        POLISHED_GRANITE_WALL = new ColoredWall("polished_granite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_GRANITE_WALL, FabriBlocks.MOD_ID);
-//        POLISHED_DIORITE_WALL = new ColoredWall("polished_diorite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_DIORITE_WALL, FabriBlocks.MOD_ID);
-//        POLISHED_ANDESITE_WALL = new ColoredWall("polished_andesite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_ANDESITE_WALL, FabriBlocks.MOD_ID);
+        POLISHED_GRANITE_WALL = new ColoredWall("polished_granite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_GRANITE_WALL, FabriBlocks.MOD_ID);
+        POLISHED_DIORITE_WALL = new ColoredWall("polished_diorite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_DIORITE_WALL, FabriBlocks.MOD_ID);
+        POLISHED_ANDESITE_WALL = new ColoredWall("polished_andesite_wall", com.yanis48.fabriblocks.init.ModBlocks.POLISHED_ANDESITE_WALL, FabriBlocks.MOD_ID);
 //        QUARTZ_WALL = new ColoredWall("quartz_wall", com.yanis48.fabriblocks.init.ModBlocks.QUARTZ_WALL, FabriBlocks.MOD_ID);
-//        PRISMARINE_BRICK_WALL = new ColoredWall("prismarine_brick_wall", com.yanis48.fabriblocks.init.ModBlocks.PRISMARINE_BRICK_WALL, FabriBlocks.MOD_ID);
-//        DARK_PRISMARINE_WALL = new ColoredWall("dark_prismarine_wall", com.yanis48.fabriblocks.init.ModBlocks.DARK_PRISMARINE_WALL, FabriBlocks.MOD_ID);
-//        PURPUR_WALL = new ColoredWall("purpur_wall", com.yanis48.fabriblocks.init.ModBlocks.PURPUR_WALL, FabriBlocks.MOD_ID);
+        PRISMARINE_BRICK_WALL = new ColoredWall("prismarine_brick_wall", com.yanis48.fabriblocks.init.ModBlocks.PRISMARINE_BRICK_WALL, FabriBlocks.MOD_ID);
+        DARK_PRISMARINE_WALL = new ColoredWall("dark_prismarine_wall", com.yanis48.fabriblocks.init.ModBlocks.DARK_PRISMARINE_WALL, FabriBlocks.MOD_ID);
+        PURPUR_WALL = new ColoredWall("purpur_wall", com.yanis48.fabriblocks.init.ModBlocks.PURPUR_WALL, FabriBlocks.MOD_ID);
 
 //        SCUTE_BLOCK.createBlocks();
 //        BAMBOO_BLOCK.createBlocks();
@@ -72,7 +72,7 @@ public class ModBlocks {
         PURPUR_WALL.createBlocks();
     }
 
-//    private static Block getID(String id) {
-//        return Registry.BLOCK.get(new Identifier(FabriBlocks.MOD_ID,id));
-//    }
+    private static Block getID(String id) {
+        return Registry.BLOCK.get(new Identifier(FabriBlocks.MOD_ID,id));
+    }
 }
--- src/main/java/arthurbambou/paintingmod/mainmod/mixins/MixinFenceBlock.java
+++ src/main/java/arthurbambou/paintingmod/mainmod/mixins/MixinFenceBlock.java
@@ -5,7 +5,7 @@ import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 
-import static net.minecraft.block.FenceBlock.method_10185;
+import static net.minecraft.block.FenceBlock.preventsConnection;
 
 @Mixin(FenceBlock.class)
 public class MixinFenceBlock extends Block {
@@ -18,10 +18,10 @@ public class MixinFenceBlock extends Block {
      * @author arthurbambou
      */
     @Overwrite
-    public boolean method_10184(BlockState blockState_1, boolean boolean_1, Direction direction_1) {
+    public boolean canConnect(BlockState blockState_1, boolean boolean_1, Direction direction_1) {
         Block block_1 = blockState_1.getBlock();
         boolean boolean_2 = block_1 instanceof FenceBlock && blockState_1.getMaterial() == this.material;
         boolean boolean_3 = block_1 instanceof FenceGateBlock && FenceGateBlock.canWallConnect(blockState_1, direction_1);
-        return !method_10185(block_1) && boolean_1 || boolean_2 || boolean_3;
+        return !preventsConnection(block_1) && boolean_1 || boolean_2 || boolean_3;
     }
 }
