cdeed17dd9ed0758927428c371196fa1c5164b80
Cat Core <arthur.koalas@free.fr>
2019-04-11 19:13:48 +0200

    Working 1.14 Pre-Release

--- src/main/java/fr/arthurbambou/paintingmod/compat/rei/PaintingModPlugin.java
+++ src/main/java/fr/arthurbambou/paintingmod/compat/rei/PaintingModPlugin.java
@@ -7,6 +7,7 @@ import fr.arthurbambou.paintingmod.mainmod.registery.ModItems;
 import me.shedaniel.rei.api.ItemRegistry;
 import me.shedaniel.rei.api.REIPlugin;
 import me.shedaniel.rei.api.RecipeHelper;
+import net.minecraft.block.Block;
 import net.minecraft.util.Identifier;
 
 public class PaintingModPlugin implements REIPlugin {
@@ -32,22 +33,13 @@ public class PaintingModPlugin implements REIPlugin {
         recipeHelper.registerDisplay(HAMMER, new DisplayHammer());
         for(ColoredObject coloredObject : PaintingModRegistry.getColoredObjectList().values()) {
             recipeHelper.registerDisplay(HEATGUN, new DisplayHeatGun(coloredObject));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.BLACK_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.black.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.RED_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.red.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.GREEN_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.green.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.BROWN_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.brown.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.BLUE_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.blue.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.PURPLE_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.purple.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.CYAN_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.cyan.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.LIGHT_GRAY_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.lightgray.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.GRAY_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.gray.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.PINK_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.pink.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.LIME_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.lime.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.YELLOW_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.yellow.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.LIGHT_BLUE_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.lightblue.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.MAGENTA_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.magenta.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.ORANGE_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.orange.getItem().getDefaultStack()));
-            recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(ModItems.WHITE_PAINTBRUSH.getDefaultStack(), coloredObject.replace.getItem().getDefaultStack(), coloredObject.white.getItem().getDefaultStack()));
+            for (int a = 0; a < coloredObject.getArrayList().size(); a++) {
+                recipeHelper.registerDisplay(PAINTBRUSH, new DisplayPaintBrush(
+                        ModItems.PAINTBRUSHES.get(a + 1).getDefaultStack(),
+                        coloredObject.replace.getItem().getDefaultStack(),
+                        coloredObject.getArrayList().get(a).getItem().getDefaultStack()
+                ));
+            }
         }
     }
 
